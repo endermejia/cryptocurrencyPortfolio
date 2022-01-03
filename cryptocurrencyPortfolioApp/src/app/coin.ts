e681d1d8728703d1402681ee5cd82e1b86be31b4
Gabriel Mejia <endermejia032@gmail.com>
2022-01-03 19:57:26 +0100

    FEATURE: interfaces

---
+++ cryptocurrencyPortfolioApp/src/app/coin.ts
@@ -0,0 +1,5 @@
+export interface Coin {
+  id: number;
+  acronym: string;
+  name: string;
+}
---
+++ cryptocurrencyPortfolioApp/src/app/line.ts
@@ -0,0 +1,6 @@
+export interface Line {
+  id: number;
+  portfolioId: number;
+  coinId: number;
+  amount: number;
+}
