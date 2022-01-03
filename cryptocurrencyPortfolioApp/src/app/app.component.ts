ba903994b6cae9d4ce59acc4d9f6687bf14dcc4f
Gabriel Mejia <endermejia032@gmail.com>
2022-01-03 19:57:47 +0100

    FEATURE: services

--- cryptocurrencyPortfolioApp/src/app/app.component.ts
+++ cryptocurrencyPortfolioApp/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Coin } from './coin'
+import { Line } from "./line";
+import { Portfolio } from "./portfolio";
+import {CryptocurrencyService} from "./cryptocurrency.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'cryptocurrencyPortfolioApp';
+  title = 'Cryptocurrency Portfolio';
+  coins: Coin[] = [];
+  portfolios: Portfolio[] = [];
+  lines: Line[] = [];
+  realCoinsList: string[] = [];
+
+  constructor(private cryptocurrencyService: CryptocurrencyService) { }
+
+  ngOnInit(): void {
+    this.getCoins();
+    this.getLines();
+    this.getPortfolios();
+    this.getRealCoins();
+  }
+
+  getCoins(): void {
+    this.cryptocurrencyService.getCoins()
+      .subscribe((coins) => this.coins = coins);
+  }
+
+  getLines(): void {
+    this.cryptocurrencyService.getLines()
+      .subscribe((lines) => this.lines = lines);
+  }
+
+  getPortfolios(): void {
+    this.cryptocurrencyService.getPortfolios()
+      .subscribe((portfolios) => this.portfolios = portfolios);
+  }
+
+  getRealCoins(): void {
+    this.cryptocurrencyService.getCryptoCompareData()
+      .subscribe(response => {
+        if (response && response.Data) {
+          this.realCoinsList = Object.keys(response.Data);
+          this.coins.filter(coin => this.realCoinsList.includes(coin.acronym));
+        }
+      });
+  }
+
+  realCoin(coinName: string): boolean {
+    return this.realCoinsList.some(realCoin => realCoin === coinName);
+  }
+
 }
---
+++ cryptocurrencyPortfolioApp/src/app/cryptocurrency.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of} from "rxjs";
+import { Coin } from "./coin";
+import { Line } from "./line";
+import { Portfolio } from "./portfolio";
+import {catchError, tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CryptocurrencyService {
+
+  private dataBaseUrl = 'http://localhost:3000/';
+  private coinsUrl = this.dataBaseUrl + 'coins';
+  private linesUrl = this.dataBaseUrl + 'lines';
+  private portfolioUrl = this.dataBaseUrl + 'portfolios';
+  private cryptoCompareUrl = 'https://min-api.cryptocompare.com/data/all/coinlist';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCoins(): Observable<Coin[]> {
+    return this.http.get<Coin[]>(this.coinsUrl)
+      .pipe(
+        tap(_ => console.log('fetched coins')),
+        catchError(this.handleError<Coin[]>('getCoins', []))
+      );
+  }
+
+  getLines(): Observable<Line[]> {
+    return this.http.get<Line[]>(this.linesUrl)
+      .pipe(
+        tap(_ => console.log('fetched lines')),
+        catchError(this.handleError<Line[]>('getLines', []))
+      );
+  }
+
+  getPortfolios(): Observable<Portfolio[]> {
+    return this.http.get<Portfolio[]>(this.portfolioUrl)
+      .pipe(
+        tap(_ => console.log('fetched portfolios')),
+        catchError(this.handleError<Portfolio[]>('getPortfolios', []))
+      );
+  }
+
+  getCryptoCompareData(): Observable<any> {
+    return this.http.get(this.cryptoCompareUrl)
+      .pipe(
+        tap(_ => console.log('fetched cryptoCompare data')),
+        catchError(this.handleError('getCryptoCompareData', []))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+}
